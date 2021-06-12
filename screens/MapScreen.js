94c6a1decce5aab234fa8ae8144511d1f4579342
AnimatorJoe <josephjin7@gmail.com>
2021-06-12 17:37:02 -0500

    adding search bar

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,5 +1,18 @@
 import React, { useState, useEffect } from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import {
+  Keyboard,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  View
+} from "react-native";
+import {
+  Button,
+  Colors,
+  IconButton,
+  Searchbar,
+  Surface
+} from "react-native-paper";
 import MapView from "react-native-maps";
 import * as Location from "expo-location";
 
@@ -7,9 +20,9 @@ const MapScreen = props => {
   const [location, setLocation] = useState(null);
   const [region, setRegion] = useState({
     latitude: 37.78825,
-    longitude: -122.4324,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421
+    longitude: -95.4324,
+    latitudeDelta: 40,
+    longitudeDelta: 80
   });
 
   // ask for user permission and get location upon acceptance
@@ -23,34 +36,80 @@ const MapScreen = props => {
 
       let location = await Location.getCurrentPositionAsync({});
       setLocation(location);
-    })();
-  }, []);
-
-  // if location is obtained, use it
-  useEffect(() => {
-    if (location) {
       setRegion({
         latitude: location.coords.latitude,
         longitude: location.coords.longitude,
         latitudeDelta: 0.1,
         longitudeDelta: 0.05
       });
-    }
-  }, [location]);
+    })();
+  }, []);
 
   return (
-    <MapView
-      showsUserLocation={true}
-      region={region}
-      style={styles.mapView}
-    ></MapView>
+    <View style={styles.container}>
+      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <MapView
+          showsUserLocation={true}
+          showsCompass={false}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -95.4324,
+            latitudeDelta: 40,
+            longitudeDelta: 80
+          }}
+          region={region}
+          style={styles.mapView}
+        ></MapView>
+      </TouchableWithoutFeedback>
+      <Surface style={styles.searchbarContainer}>
+        <Searchbar
+          placeholder='Search here'
+          style={styles.searchBar}
+          iconColor={Colors.grey900}
+        ></Searchbar>
+        <IconButton
+          icon='cog-outline'
+          color={Colors.grey900}
+          size={20}
+          onPress={() => console.log("Pressed")}
+        />
+      </Surface>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    alignSelf: "stretch",
+    height: "100%"
+  },
   mapView: {
     alignSelf: "stretch",
     height: "100%"
+  },
+  searchbarContainer: {
+    height: 60,
+    width: "92%",
+    flexDirection: "row",
+    alignSelf: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+    position: "absolute",
+    top: 50,
+    borderRadius: 15,
+    elevation: 2
+  },
+  searchBar: {
+    flex: 1,
+    width: "95%",
+    alignSelf: "stretch",
+    borderRadius: 15,
+    elevation: 0
+  },
+  settingsButton: {
+    alignSelf: "stretch",
+    width: 60
   }
 });
 
